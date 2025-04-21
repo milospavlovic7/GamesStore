d9a51ace1d9880cadfa3a2f7f64505b2f26a92fd
milospavlovic7 <milos.pavlovic0704@gmail.com>
2025-04-21 16:44:53 +0200

    dtos

--- GamesStore/Program.cs
+++ GamesStore/Program.cs
@@ -1,3 +1,5 @@
+using GamesStore.Api.DTOs;
+
 namespace GamesStore.Api
 {
     public class Program
@@ -7,7 +9,42 @@ namespace GamesStore.Api
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
+            const string GetGameEndpointName = "GetGame";
+
+            List<GameDto> games = [
+                new (
+                    1,
+                    "Fifa 14",
+                    "Sport",
+                    59.99M,
+                    new DateOnly(2014,8,21)),
+                new (
+                    2,
+                    "GTA V",
+                    "IDK",
+                    100M,
+                    new DateOnly(2011,5,5))
+                ];
+
+            app.MapGet("games", () => games);
+
             app.MapGet("/", () => "Hello World!");
+
+            app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
+
+            app.MapPost("games", (CreateGameDto newGame) =>
+            {
+                GameDto game = new(
+                    games.Count + 1,
+                    newGame.Name,
+                    newGame.Genre,
+                    newGame.Price,
+                    newGame.ReleaseDate);
+
+                games.Add(game);
+
+                return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+            });
 
             app.Run();
         }
